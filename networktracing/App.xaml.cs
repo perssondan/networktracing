620ffa8dd0cc9bd89a954c84a29375b7328646d2
Dan Persson <dan.persson@cenito.se>
2019-11-25 07:53:35 +0100

    Added test project

---
+++ networktracing/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime;
+using System.Windows;
+
+namespace networktracing
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            ProfileOptimization.SetProfileRoot(Environment.GetEnvironmentVariable("temp"));
+            ProfileOptimization.StartProfile("networking.startup.profile");
+        }
+    }
+}
---
+++ networktracing/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Windows;
+
+namespace networktracing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://www.google.com");
+            }
+        }
+    }
+}
